39b24532e5b7a5fb89c40fee8336666b75008d15
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2023-07-07 02:06:12 -0300

    2 15 ERROS

--- src/services/enrollments-service/index.ts
+++ src/services/enrollments-service/index.ts
@@ -1,10 +1,10 @@
 import { Address, Enrollment } from '@prisma/client';
 import { request } from '@/utils/request';
-import { AddressObj, AddressEnrollment } from '@/protocols';
-import { NoContentError, notFoundError } from '@/errors';
+import { invalidDataError, notFoundError } from '@/errors';
 import addressRepository, { CreateAddressParams } from '@/repositories/address-repository';
 import enrollmentRepository, { CreateEnrollmentParams } from '@/repositories/enrollment-repository';
 import { exclude } from '@/utils/prisma-utils';
+import { AddressEnrollment } from '@/protocols';
 
 async function getAddressFromCEP(cep: string): Promise<AddressEnrollment> {
   const result = await request.get(`${process.env.VIA_CEP_API}/${cep}/json/`);
@@ -25,51 +25,6 @@ async function getAddressFromCEP(cep: string): Promise<AddressEnrollment> {
 
   return address;
 }
-
-async function createOrUpdateEnrollmentWithAddress(params: CreateOrUpdateEnrollmentWithAddress) {
-  const address = getAddressForUpsert(params.address);
-  const cep = params.address.cep.replace(/-/g, '');
-
-  const birthday = params.birthday.toString();
-  const [year, month, day] = birthday.split('-');
-
-  const dataString = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
-  if (isNaN(dataString.getTime())) return;
-
-  const body = {
-    name: params.name,
-    cpf: params.cpf,
-    birthday: dataString,
-    phone: params.phone,
-    userId: params.userId,
-    address: {
-      cep: cep,
-      street: params.address.street,
-      city: params.address.city,
-      number: params.address.number,
-      state: params.address.state,
-      neighborhood: params.address.neighborhood,
-      addressDetail: params.address.addressDetail,
-    },
-  };
-
-  await getAddressFromCEP(address.cep);
-  const enrollment = { ...exclude(params, 'address') };
-  const newEnrollment = await enrollmentRepository.upsert(body.userId, enrollment, exclude(enrollment, 'userId'));
-
-  await addressRepository.upsert(newEnrollment.id, address, address);
-}
-
-function getAddressForUpsert(address: CreateAddressParams) {
-  return {
-    ...address,
-    ...(address?.addressDetail && { addressDetail: address.addressDetail }),
-  };
-}
-
-export type CreateOrUpdateEnrollmentWithAddress = CreateEnrollmentParams & {
-  address: CreateAddressParams;
-};
 
 async function getOneWithAddressByUserId(userId: number): Promise<GetOneWithAddressByUserIdResult> {
   const enrollmentWithAddress = await enrollmentRepository.findWithAddressByUserId(userId);
