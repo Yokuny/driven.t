bc16359e85aad63724d108f6bb6ac8fea5510839
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2023-07-14 10:40:30 -0300

    to rebase

--- src/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './users-schemas';
-export * from './authentication-schemas';
-export * from './enrollments-schemas';
-export * from './ticket-schemas';
-export * from './payments-schemas';
