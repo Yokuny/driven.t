f2b39e08ddb1697307671417b3b5a9a61c7c89ef
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2023-07-24 10:09:07 -0300

    export fix

--- src/routers/hotels-router.ts
+++ src/routers/hotels-router.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import { authenticateToken } from '@/middlewares';
-import { getHotels, getHotelsWithRooms } from '@/controllers/hotel-controller';
+import { getHotels, getRooms } from '@/controllers/hotels-controller';
 
 const hotelsRouter = Router();
 
-hotelsRouter.all('/*', authenticateToken).get('/', getHotels).get('/:hotelId', getHotelsWithRooms);
+hotelsRouter.all('/*', authenticateToken).get('/', getHotels).get('/:hotelId', getRooms);
 
 export { hotelsRouter };
